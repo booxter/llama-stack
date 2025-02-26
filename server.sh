533108a890322f412c31119f2be4bafab9b48340
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-02-26 21:37:50 +0000

    server.sh - handle different locations of python3.11 better...
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- server.sh
+++ server.sh
@@ -3,7 +3,11 @@
 set -ex
 
 export INFERENCE_MODEL=meta-llama/Llama-3.2-3B-Instruct
-PYTHON=$(which python3.11)
+if [ -e /usr/bin/python3.11 ]; then
+  PYTHON=/usr/bin/python3.11
+else
+  PYTHON=$(which python3.11)
+fi
 LLS_VENV=train
 TEMPLATE=experimental-post-training
 CONFIG=~/.llama/distributions/$TEMPLATE/$TEMPLATE-run.yaml
@@ -24,5 +28,12 @@ fi
 
 export UV_PYTHON=$(which python)
 llama stack build --template $TEMPLATE --image-type venv --image-name $LLS_VENV
+
+./venv/bin/python -m ensurepip
+./venv/bin/pip install kfp==2.12
+
+./$LLS_VENV/bin/python -m ensurepip
+./$LLS_VENV/bin/pip3 install kfp==2.12
+
 llama stack run --image-type venv --image-name venv $CONFIG
 
