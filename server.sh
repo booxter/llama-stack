373a846962d17545b1084a084d7e3e4c16222e5f
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-04-09 01:18:08 +0000

    add train script
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- server.sh
+++ server.sh
@@ -8,31 +8,22 @@ if [ -e /usr/bin/python3.11 ]; then
 else
   PYTHON=$(which python3.11)
 fi
-LLS_VENV=train
 TEMPLATE=experimental-post-training
 CONFIG=~/.llama/distributions/$TEMPLATE/$TEMPLATE-run.yaml
 
-if [ x$1 = x--clean -o ! -f venv ]; then
-  rm -rf venv $LLS_VENV
+if [ x$1 = x--clean -o ! -d venv ]; then
+  rm -rf venv
   $PYTHON -m venv venv
 fi
 . ./venv/bin/activate
 pip install -e .
+pip install uv
 
 # Load model if not already
 echo test | ollama run llama3.2:3b-instruct-fp16 --keepalive -1m
 
 if ! [ -e ~/.llama/checkpoints/ ]; then
-  llama download --source meta --model-id  Llama3.2-3B-Instruct
+  llama download --source meta --model-id Llama3.2-3B-Instruct
 fi
 
-export UV_PYTHON=$(which python)
-llama stack build --template $TEMPLATE --image-type venv --image-name $LLS_VENV
-
-./venv/bin/python -m ensurepip
-./venv/bin/pip install kfp==2.12
-
-./$LLS_VENV/bin/python -m ensurepip
-./$LLS_VENV/bin/pip3 install kfp==2.12
-
-llama stack run --image-type venv --image-name venv $CONFIG
+llama stack build --image-type venv --run --template $TEMPLATE
